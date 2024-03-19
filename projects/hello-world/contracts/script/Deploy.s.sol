635b58ea4479f2e844a36f34cfec6672def8cc1e
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2024-03-19 20:50:04 +0800

    Update Deploy.s.sol

--- projects/hello-world/contracts/script/Deploy.s.sol
+++ projects/hello-world/contracts/script/Deploy.s.sol
@@ -14,7 +14,7 @@ contract Deploy is Script {
         address deployerAddress = vm.addr(deployerPrivateKey);
         console2.log("Loaded deployer: ", deployerAddress);
 
-        address coordinator = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
+        address coordinator = 0x8d871ef2826ac9001fb2e33fdd6379b6aabf449c;
         // Create consumer
         SaysGM saysGm = new SaysGM(coordinator);
         console2.log("Deployed SaysHello: ", address(saysGm));
